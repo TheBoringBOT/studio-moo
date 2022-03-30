9d704098c566b4f897fbc9105551fbfd30abd28a
TheBoringBot <richardsgareth@icloud.com>
2022-03-30 16:56:16 +0200

    framer moition page transitions added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "../styles/index.css";
 import { ThemeProvider } from "next-themes";
+import { motion } from "framer-motion";
+import { pageVariants } from "../lib/animation-constants";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />;
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={pageVariants}
+      >
+        <Component {...pageProps} />;
+      </motion.div>
     </ThemeProvider>
   );
 }
