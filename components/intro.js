ade43de1677bab2735659deb35e39d67a19bd394
TheBoringBot <richardsgareth@icloud.com>
2022-03-30 12:56:59 +0200

    Content text changed

--- components/intro.js
+++ components/intro.js
@@ -1,21 +1,24 @@
-import { CMS_NAME } from '../lib/constants'
+import {
+  SITE_NAME,
+  SITE_BLOG_TITLE,
+  SITE_BLOG_DESCRIPTION,
+} from "../lib/constants";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+    <section className="flex-col lg:flex-row flex items-baseline lg:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-left text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        {SITE_BLOG_TITLE}
+        <br />
+        <p className="invisible md:visible  text-sm tracking-wide md:mt-[-20px] md:pl-2">
+          Av {SITE_NAME}.
+        </p>
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+      <h4 className="text-left text-lg mt-5 lg:pl-8">
+        {SITE_BLOG_DESCRIPTION}
+        <br />
+        <span className="md:hidden">Av {SITE_NAME}.</span>
       </h4>
     </section>
-  )
+  );
 }
