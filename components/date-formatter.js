fbac56ab2cefb9a62aa59f9962069c3cd556515b
TheBoringBot <richardsgareth@icloud.com>
2022-03-30 12:23:50 +0200

    new get year function

--- components/date-formatter.js
+++ components/date-formatter.js
@@ -1,6 +1,11 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
 
 export default function DateFormatter({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
 }
+
+export const getFullYear = () => {
+  const date = new Date();
+  return date.getFullYear();
+};
