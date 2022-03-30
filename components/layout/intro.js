e7db36411b2c956b2a8b1b6f6bddd9c918005cd4
TheBoringBot <richardsgareth@icloud.com>
2022-03-30 15:27:24 +0200

    dark mode toggle added

--- components/layout/intro.js
+++ components/layout/intro.js
@@ -3,22 +3,25 @@ import {
   SITE_BLOG_TITLE,
   SITE_BLOG_DESCRIPTION,
 } from "../../lib/constants";
+import ThemeToggle from "./theme-toggle";
 
 export default function Intro() {
   return (
-    <section className="flex-col lg:flex-row flex items-baseline lg:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-left text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        {SITE_BLOG_TITLE}
-        <br />
-        <p className="invisible md:visible  text-sm tracking-wide md:mt-[-20px] md:pl-2">
-          Av {SITE_NAME}.
-        </p>
-      </h1>
-      <h4 className="text-left text-lg mt-5 lg:pl-8">
-        {SITE_BLOG_DESCRIPTION}
-        <br />
-        <span className="md:hidden">Av {SITE_NAME}.</span>
-      </h4>
+    <section className="flex-col   lg:flex-row flex items-baseline lg:justify-between mt-16 mb-16 md:mb-12">
+      <div className="flex-col">
+        <h1 className="text-left text-7xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          {SITE_BLOG_TITLE}
+          <br />
+          <p className="  text-sm tracking-wide md:mt-[-20px] md:pl-2">
+            Av {SITE_NAME}.
+          </p>
+        </h1>
+      </div>
+      <div className="flex-col items-center">
+        <div className="flex md:justify-end  mt-5">
+          <ThemeToggle iconWidth={30} />
+        </div>
+      </div>
     </section>
   );
 }
