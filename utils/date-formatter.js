6aeb613c8d804e5d98a92b3c5033bd4445bf6a40
TheBoringBot <richardsgareth@icloud.com>
2022-04-01 12:40:22 +0200

    locale date formatting

--- utils/date-formatter.js
+++ utils/date-formatter.js
@@ -1,11 +1,25 @@
-import { parseISO, format } from "date-fns";
+import { parseISO, format, formatRelative, subDays } from "date-fns";
+import { se } from "date-fns/locale";
 
-export default function DateFormatter({ dateString }) {
+// uses locale to return correct date format
+export default function DateFormatter({ dateString, formatStr = "PP" }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>;
+  return (
+    <time dateTime={dateString}>{format(date, formatStr, { locale: se })}</time>
+  );
 }
 
 export const getFullYear = () => {
-  const date = new Date();
+  const date = new Date({ locale: se });
   return date.getFullYear();
+};
+
+// show how long since post was updated
+export const PostWasUpdatedAt = (dateString) => {
+  // returns in swedish format
+  // return formatRelative(subDays(new Date(), 3), new Date(), {
+  //   locale: se,
+  // });
+  return "fish";
+  //=> "last Friday at 7:26 p.m."
 };
