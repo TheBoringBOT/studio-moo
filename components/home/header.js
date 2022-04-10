3bea5d3a6992cf8fd6863ce805a8a92265a37cc3
TheBoringBot <richardsgareth@icloud.com>
2022-04-10 12:26:54 +0200

    style improvements

--- components/home/header.js
+++ components/home/header.js
@@ -24,26 +24,32 @@ export default function Header({}) {
           <nav>
             <ul className="flex justify-end items-center space-x-6 uppercase ">
               <Link href="/blog">
-                <a className=" duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px]">
+                <a className=" duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px] hover:scale-102   ">
                   Om Oss
                 </a>
               </Link>
               <Link href="/blog">
-                <a className=" duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px]">
+                <a className=" duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px] hover:scale-102 ">
                   Tjänster
                 </a>
               </Link>
               <Link href="/blogg">
-                <a className="duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px]">
+                <a className="duration-200 transition-all rounded-full border-dashed border-2 border-transparent hover:border-bg-primary p-2 hover:translate-y-[-5px] hover:scale-102 ">
                   Blogg
                 </a>
               </Link>
 
               <a
                 href={`mailto:${SITE_CONTACT_EMAIL}`}
-                className="mx-3 bg-black dark:bg-gray-100 hover:bg-white hover:text-black hover:dark:bg-transparent hover:dark:border-gray-100 hover:dark:text-gray-100 border border-black text-white dark:text-black font-bold py-2 px-2 duration-200 rounded transition-colors mb-6 lg:mb-0"
+                className="relative mx-3  py-2 px-2 duration-200 transition-all rounded
+                 mb-6 lg:mb-0 text-white  flex items-center justify-center h-28 overflow-hidden text-lg hover:translate-y-[-5px] hover:scale-102 "
               >
-                Mejla Oss
+                <img
+                  className="z-[-1] absolute top-0 left-0 right-0 bottom-0 h-full w-full spin "
+                  src="/assets/shapes/shape1.svg"
+                  alt=""
+                />
+                <span className="p-2">Kontakt</span>
               </a>
             </ul>
           </nav>
