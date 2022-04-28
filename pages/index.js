f410504b2b55fb5735c203f5d604943a9464d60b
TheBoringBot <richardsgareth@icloud.com>
2022-04-28 21:43:29 +0200

    refactor: cleanup

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ import Layout from "../components/layout/layout";
 // import components
 import Hero from "../components/home/hero/hero";
 import Header from "../components/home/header";
-import ClientLogos from "../components/home/client-logos/client-logos";
-import About from "../components/home/about/about";
-import OurProcess from "../components/home/our-process/our-process";
-import Work from "../components/home/work/work";
+
+// smooth scroll
+import LocomotiveScroll from "../components/home/locomotive-scroll";
 
 export default function Index() {
   return (
+    // <LocomotiveScroll>
     <div className="grain">
       <Layout>
         <Head>
@@ -22,14 +22,10 @@ export default function Index() {
             {SITE_NAME} | {SITE_DESCRIPTION_SHORT}
           </title>
         </Head>
-        <Header />
 
         <Hero />
-        <ClientLogos />
-        <About />
-        {/* <OurProcess />
-        <Work /> */}
       </Layout>
     </div>
+    // </LocomotiveScroll>
   );
 }
