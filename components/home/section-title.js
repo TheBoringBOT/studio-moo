5f64a831d14fe10172378fab6d4bb6d435513c8e
TheBoringBot <richardsgareth@icloud.com>
2022-04-28 22:28:29 +0200

    feat: prop to override width

--- components/home/section-title.js
+++ components/home/section-title.js
@@ -1,13 +1,13 @@
-export default function sectionTitle({ children, h1, h2, h3, p }) {
+export default function sectionTitle({ children, h1, h2, h3, p, widthRemove }) {
   return (
-    <div className=" mt-28 w-[800px] max-w-[90vw] mx-auto">
-      <div className="flex flex-col items-center justify-center space-y-8 leading-none tracking-tight text-center pt-2">
+    <div
+      className={` ${!widthRemove && "w-[800px]"} mt-28  max-w-[90vw] mx-auto`}
+    >
+      <div className="flex flex-col items-center justify-center pt-2 space-y-8 leading-none tracking-tight text-center">
         {/* title */}
-        <div className="text-black title-normal">
-          {h1 && (
-            <h1 className="text-[3.6rem] md:text-[5.3rem] font-bold">{h1}</h1>
-          )}
-          {h2 && <h2 className="text-[2.1rem] md:text-[3.8rem]">{h2}</h2>}
+        <div className="font-bold text-black title-normal">
+          {h1 && <h1 className="text-[3.6rem] md:text-[5.3rem] ">{h1}</h1>}
+          {h2 && <h2 className="text-[3.6rem] md:text-[5.3rem]">{h2}</h2>}
           {h3 && <h3 className="text-[2.1rem] md:text-[4.1rem]">{h3}</h3>}
         </div>
         {/* subtitle */}
