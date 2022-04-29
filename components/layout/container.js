1bb673e672ac273d1042398727e2a2827aa03b06
TheBoringBot <richardsgareth@icloud.com>
2022-04-29 11:18:17 +0200

    style: removed padding-x  on lg screens

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className="container max-w-6xl px-10 mx-auto bg-white space-y-28">
+    <div className="container max-w-6xl px-10 lg:px-0 mx-auto bg-white space-y-28">
       {children}
     </div>
   );
