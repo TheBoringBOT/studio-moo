c5814b68da3afacc969cadd0209245c80d1268d3
TheBoringBot <richardsgareth@icloud.com>
2022-04-29 09:16:10 +0200

    feat: hook for scrollY position

---
+++ hooks/useScrollPosition.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+//get the scrollY position
+// modified version of https://designcode.io/react-hooks-handbook-usescrollposition-hook
+
+const useScrollPosition = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPosition(window.scrollY);
+    };
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return scrollPosition;
+};
+
+export default useScrollPosition;
