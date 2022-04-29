7df95317325e9d15e8d694b99cd2f1753e7f045d
TheBoringBot <richardsgareth@icloud.com>
2022-04-29 09:14:52 +0200

    feat: about section structure with lorem ipsum

---
+++ components/home/about/about.js
@@ -0,0 +1,21 @@
+import SectionTitle from "../section-title";
+import Spacer from "../../layout/spacer";
+export default function about() {
+  return (
+    <div>
+      <Spacer />
+      <SectionTitle
+        widthRemove={true}
+        h2={<>Sit viverra nullam quis. Ullamcorper lacus.</>}
+        p={
+          <>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam tempus
+            morbi amet elementum sapien. Ac non lacinia vulputate est suscipit.
+            Elit ipsum elit sit consectetur tristique viverra quam cursus
+            egestas.
+          </>
+        }
+      ></SectionTitle>
+    </div>
+  );
+}
