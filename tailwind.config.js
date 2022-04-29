500e6feab7b01dea24e2916abf14cf5b46ad4ab2
TheBoringBot <richardsgareth@icloud.com>
2022-04-29 09:54:04 +0200

    style: custom heights for grid cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,10 @@ module.exports = {
         sm: "0 5px 10px rgba(0, 0, 0, 0.12)",
         md: "0 8px 30px rgba(0, 0, 0, 0.12)",
       },
+      height: {
+        "card-lg": "800px",
+        "card-sm": "600px",
+      },
     },
   },
 };
