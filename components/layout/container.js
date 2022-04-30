9604324278a2081008b657af8cc55881a44ab0e4
TheBoringBot <richardsgareth@icloud.com>
2022-04-30 11:51:45 +0200

    style: outer & inner wrapper now with background white

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,7 +1,9 @@
 export default function Container({ children }) {
   return (
-    <div className="container relative z-10 max-w-6xl px-10 mx-auto bg-white lg:px-0 space-y-28">
-      {children}
+    <div className="relative z-10 w-full bg-white ">
+      <div className="max-w-6xl px-10 mx-auto lg:px-0 space-y-28">
+        {children}
+      </div>
     </div>
   );
 }
