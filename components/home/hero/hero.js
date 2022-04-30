579f46a095810f3aea9b09852db8afa4969547e1
TheBoringBot <richardsgareth@icloud.com>
2022-04-30 11:51:24 +0200

    fix: parallax scroll fix janky movement

--- components/home/hero/hero.js
+++ components/home/hero/hero.js
@@ -4,6 +4,8 @@ import SectionTitle from "../section-title";
 import Header from "./header";
 import UseScrollPosition from "../../../hooks/useScrollPosition";
 
+
+
 export default function hero() {
   const scrollPosition = UseScrollPosition();
   const containerRef = useRef(null);
@@ -13,51 +15,53 @@ export default function hero() {
   // parallax on scroll from scrollY
 
   if (elCurrent) {
-    scrollProgress = (scrollPosition / elCurrent.clientHeight) * 120;
-    opacityProgress = 1 - (scrollPosition / elCurrent.clientHeight) * 1.2;
+    // scrollProgress = (scrollPosition / elCurrent.clientHeight) * 120;
+    opacityProgress = 1 - Math.min(1, scrollPosition / elCurrent.clientHeight);
+    scrollProgress = Math.min(1, scrollPosition / elCurrent.clientHeight);
   }
   return (
-    <div ref={containerRef}>
+    <div
+      ref={containerRef}
+      className="bg-mid-grey sticky top-0  min-h-[690px] h-[72vh]
+      max-h-[80vh] md:max-h-full "
+    >
+      {/* Parallax div */}
       <div
-        className="bg-mid-grey sticky top-0  min-h-[600px] h-[80vh]
-      max-h-[770px] "
+        className="will-change-transform"
+        style={{ transform: `translateY(${scrollProgress * 35}vh)` }}
       >
-        {/* Parallax div */}
         <div
-          className="will-change-transform"
-          style={{ transform: `translateY(${scrollProgress}%)` }}
+          style={{
+            opacity: opacityProgress,
+          }}
+          className="flex flex-col items-center justify-center"
         >
-          <div
-            style={{ opacity: opacityProgress }}
-            className="flex flex-col items-center justify-center"
+          <Header />
+          <SectionTitle
+            h1={
+              <>
+                <span className="text-white">
+                  Quam ut tellus blandit est et in.
+                </span>
+              </>
+            }
+            p={
+              <>
+                <span className="text-white">
+                  Odio blandit pretium scelerisque sit proin.
+                </span>
+              </>
+            }
           >
-            <Header />
-            <SectionTitle
-              h1={
-                <>
-                  <span className="text-white">
-                    Quam ut tellus blandit est et in.
-                  </span>
-                </>
-              }
-              p={
-                <>
-                  <span className="text-white">
-                    Odio blandit pretium scelerisque sit proin.
-                  </span>
-                </>
-              }
-            >
-              <div className="z-10 py-2">
-                <button className="px-8 py-4 text-lg text-white transition-all rounded-full cursor-pointer white text- bg-black-2 hover:bg-black-2-hover">
-                  Contact Us
-                </button>
-              </div>
-            </SectionTitle>
+            <div className="z-10 py-2">
+              <button className="px-8 py-4 text-lg text-white transition-all rounded-full cursor-pointer white text- bg-black-2 hover:bg-black-2-hover">
+                Contact Us
+              </button>
+            </div>
+          </SectionTitle>
 
-            {/* <div className={styles.shape1}></div>
+          {/* <div className={styles.shape1}></div>
       <div className={styles.shape2}></div> */}
-          </div>
         </div>
       </div>
     </div>
