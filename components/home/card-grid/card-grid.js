b316dd149f589ed7a6dda154e4f9bad198abb30d
TheBoringBot <richardsgareth@icloud.com>
2022-05-01 13:09:23 +0200

    feat: intial card-grid setup

--- components/home/card-grid/card-grid.js
+++ components/home/card-grid/card-grid.js
@@ -1,15 +1,13 @@
-import Spacer from "../../layout/spacer";
-import CardHalf from "./card-half";
-import CardFull from "./card-full";
+import Card from "./card";
 
 export default function CardGrid() {
   return (
     <>
       <section className=" grid  grid-cols-[repeat(2, 2fr)] gap-y-2.5 gap-x-2.5">
-        <CardFull />
-        <CardHalf />
-        <CardHalf />
-        <CardFull />
+        <Card size={2} />
+        <Card size={1} />
+        <Card size={1} delay={0.5} />
+        <Card size={2} />
       </section>
     </>
   );
