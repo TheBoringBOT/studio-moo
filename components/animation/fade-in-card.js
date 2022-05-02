48084f7261f2932dc455925ea59677428402f488
TheBoringBot <richardsgareth@icloud.com>
2022-05-02 12:33:08 +0200

    feat: opacity change 0-1 on enter

---
+++ components/animation/fade-in-card.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+//? animation component for use in other components to animate fade in on scroll when in view
+
+export default function FadeInCard({ children, delay }) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{
+        duration: 0.8,
+        delay: delay ? delay : 0,
+        ease: [0.43, 0.13, 0.23, 0.96],
+      }}
+      variants={{
+        visible: { opacity: 1, scale: 1, translateY: 0 },
+        hidden: { opacity: 0, scale: 0.98, translateY: 200 },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
