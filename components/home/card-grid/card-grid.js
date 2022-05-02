5b2bef9de6602917134022e7cff0ea7cd3e2ae2c
TheBoringBot <richardsgareth@icloud.com>
2022-05-02 14:18:32 +0200

    feat: semantic html added (Section)

--- components/home/card-grid/card-grid.js
+++ components/home/card-grid/card-grid.js
@@ -1,14 +1,18 @@
+import Container from "../../layout/container";
 import Card from "./card";
+import Layout from "../../layout/layout";
 
 export default function CardGrid() {
   return (
-    <>
-      <section className=" grid  grid-cols-[repeat(2, 2fr)] gap-y-2.5 gap-x-2.5">
-        <Card size={2} />
-        <Card size={1} />
-        <Card size={1} delay={0.5} />
-        <Card size={2} />
-      </section>
-    </>
+    <section>
+      <Container>
+        <div className=" grid  grid-cols-[repeat(2, 2fr)] gap-y-2.5 gap-x-2.5">
+          <Card size={2} />
+          <Card size={1} />
+          <Card size={1} delay={0.5} />
+          <Card size={2} />
+        </div>
+      </Container>
+    </section>
   );
 }
