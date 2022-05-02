964240d04cdeb2599924a1da7b32170c3498f573
TheBoringBot <richardsgareth@icloud.com>
2022-05-02 14:19:28 +0200

    feat: added project cta to page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { SITE_NAME, SITE_DESCRIPTION_SHORT } from "../lib/constants";
 
 // Layout components
-import Container from "../components/layout/container";
+import Spacer from "../components/layout/spacer";
 import Layout from "../components/layout/layout";
 
 // import components
@@ -10,11 +10,12 @@ import Hero from "../components/home/hero/hero";
 import About from "../components/home/about/about";
 import CardGrid from "../components/home/card-grid/card-grid";
 import Faq from "../components/home/faq/faq";
+import ProjectCta from "../components/home/cta/project-cta";
 import Footer from "../components/home/footer/footer";
 
 export default function Index() {
   return (
-    <div className="overflow-hidden bg-white">
+    <div className="bg-white ">
       <Layout>
         <Head>
           <title>
@@ -23,10 +24,12 @@ export default function Index() {
         </Head>
 
         <Hero />
-        <Container>
-          <About />
-          <CardGrid />
-        </Container>
+
+        <About />
+        <Spacer />
+        <CardGrid />
+
+        <ProjectCta />
         {/* <div className="bg-light-grey">
           <Faq />
         </div> */}
