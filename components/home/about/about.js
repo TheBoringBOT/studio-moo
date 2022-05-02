4a5239b5c715897659de0eacaaaa04d4cf59e3c6
TheBoringBot <richardsgareth@icloud.com>
2022-05-02 14:18:26 +0200

    feat: semantic html added (Section)

--- components/home/about/about.js
+++ components/home/about/about.js
@@ -1,23 +1,24 @@
 import SectionTitle from "../section-title";
+import Container from "../../layout/container";
 import Spacer from "../../layout/spacer";
 import FadeInText from "../../animation/fade-in-text";
 export default function about() {
   return (
-    <div>
-      <Spacer />
-
-      <SectionTitle
-        widthRemove={true}
-        h2={<>Sit viverra nullam quis. Ullamcorper lacus.</>}
-        p={
-          <>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam tempus
-            morbi amet elementum sapien. Ac non lacinia vulputate est suscipit.
-            Elit ipsum elit sit consectetur tristique viverra quam cursus
-            egestas.
-          </>
-        }
-      ></SectionTitle>
-    </div>
+    <section>
+      <Container>
+        <SectionTitle
+          widthRemove={true}
+          h2={<>Sit viverra nullam quis. Ullamcorper lacus.</>}
+          p={
+            <>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam
+              tempus morbi amet elementum sapien. Ac non lacinia vulputate est
+              suscipit. Elit ipsum elit sit consectetur tristique viverra quam
+              cursus egestas.
+            </>
+          }
+        ></SectionTitle>
+      </Container>
+    </section>
   );
 }
