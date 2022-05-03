cd3a91220e9606bf060f04d8559fffd9b696221a
TheBoringBot <richardsgareth@icloud.com>
2022-05-03 13:02:27 +0200

    feat: scroll progress hook for ref location

---
+++ hooks/useRefScrollProgress.js
@@ -0,0 +1,22 @@
+import { useState, useRef, useLayoutEffect } from "react";
+
+//? Hook from https://cole.codes/posts/framer-motion-useviewportscroll-element-scroll
+
+export default function useRefScrollProgress(inputRef) {
+
+
+  const ref = inputRef || useRef();
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+  useLayoutEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    const rect = ref.current.getBoundingClientRect();
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const offsetTop = rect.top + scrollTop;
+    setStart(offsetTop / document.body.clientHeight);
+    setEnd((offsetTop + rect.height) / document.body.clientHeight);
+  });
+  return { ref, start, end };
+}
